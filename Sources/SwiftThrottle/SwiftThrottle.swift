8df5f0f890a388355be70b0cfd962d2cd2da0ce8
Lakr Aream <master@233owo.com>
2021-08-02 23:17:32 +0800

    Improve Architecture

--- Sources/SwiftThrottle/SwiftThrottle.swift
+++ Sources/SwiftThrottle/SwiftThrottle.swift
@@ -1,4 +1,3 @@
-//
 //  SwiftThrottle.swift
 //  Twitter @Lakr233
 //
@@ -16,14 +15,28 @@ import Foundation
 
  */
 
-public class Throttle {
-    /// lock when dispatch job to execution
-    internal var executeLock = NSLock()
+// MARK: - THROTTLE
 
-    /// lock when setting job block item
+public class Throttle {
+    // MARK: - PROPERTY
+
+    /// Setup with these values to control the throttle behave
+    /// - minimumDelay >= 0.5 second is suggested
+    public private(set) var minimumDelay: TimeInterval
+    public private(set) var workingQueue: DispatchQueue
+
+    /// lock when dispatch job to execution
+    private var executeLock = NSLock()
+
+    /// These value controls throttle behavior
+    public private(set) var lastExecute: Date?
+    public private(set) var lastRequestWasCanceled: Bool = false
+    public private(set) var scheduled: Bool = false
+
+    /// Lock when setting jobs, required by thread safe design
     private var _assignmentLock = NSLock()
-    internal var _assignment: (() -> Void)?
-    internal var assignment: (() -> Void)? {
+    private var _assignment: (() -> Void)?
+    public private(set) var assignment: (() -> Void)? {
         set {
             _assignmentLock.lock()
             defer { _assignmentLock.unlock() }
@@ -36,12 +49,7 @@ public class Throttle {
         }
     }
 
-    /// Setup with these values to control the throttle behave
-    internal var minimumDelay: TimeInterval
-    internal var workingQueue: DispatchQueue
-    internal var lastExecute: Date?
-    internal var lastRequestWasCanceled: Bool = false
-    internal var scheduled: Bool = false
+    // MARK: - INIT
 
     /// Create a throttle
     /// - Parameters:
@@ -54,9 +62,91 @@ public class Throttle {
         workingQueue = queue
 
         #if DEBUG
-            if minimumDelay < 0.1 {
-                debugPrint("[SwiftThrottle] minimumDelay less then 0.1s will be inaccurate")
+            if minimumDelay < 0.5 {
+                // we suggest minimumDelay to be at least 0.5 second
+                debugPrint("[SwiftThrottle] "
+                    + "minimumDelay(\(minimumDelay) less then 0.5s will be inaccurate"
+                    + ", last callback not guaranteed")
             }
         #endif
+    }
+
+    // MARK: - API
+
+    /// Update property minimumDelay
+    /// - Parameter interval: in second
+    public func updateMinimumDelay(interval: Double) {
+        executeLock.lock()
+        minimumDelay = interval
+        executeLock.unlock()
+    }
+
+    /// Assign job to throttle
+    /// - Parameter job: call block
+    public func throttle(job: (() -> Void)?) {
+        realThrottle(job: job, useAssignment: false)
+    }
+
+    // MARK: - BACKEND
+
+    /// Check nothing but execute
+    /// - Parameter capturedJob: block to execute
+    private func releaseExec(capturedJob: @escaping (() -> Void)) {
+        lastExecute = Date()
+        workingQueue.async {
+            capturedJob()
+        }
+    }
+
+    /// Throttle is working here
+    /// - Parameters:
+    ///   - job: block that was required to execute
+    ///   - useAssignment: shall we overwrite assigned job?
+    private func realThrottle(job: (() -> Void)?, useAssignment: Bool) {
+        // lock down every thing when resigning job
+        executeLock.lock()
+        defer { self.executeLock.unlock() }
+
+        // if called from rescheduled job, cancel job overwrite
+        var capturedJobDecision: (() -> Void)?
+        if !useAssignment {
+            // resign job every time calling from user
+            assignment = job
+            capturedJobDecision = job
+        } else {
+            capturedJobDecision = assignment
+        }
+        guard let capturedJob = capturedJobDecision else { return }
+
+        // MARK: LOCK BEGIN
+
+        if let lastExec = lastExecute {
+            // executed before, value negative
+            let timeBetween = -lastExec.timeIntervalSinceNow
+
+            if timeBetween < minimumDelay {
+                // The throttle will be reprogrammed once for future execution
+                lastRequestWasCanceled = true
+                if !scheduled {
+                    scheduled = true
+                    let dispatchTime = Double(minimumDelay - timeBetween + 0.01)
+                    // Preventing trigger failures
+                    // This is where the inaccuracy comes from
+                    workingQueue.asyncAfter(deadline: .now() + dispatchTime) {
+                        self.realThrottle(job: nil, useAssignment: true)
+                        self.scheduled = false
+                    }
+                }
+            } else {
+                // Throttle release to execution
+                releaseExec(capturedJob: capturedJob)
+            }
+        }
+        else // never called before, release to execution
+        {
+            releaseExec(capturedJob: capturedJob)
+        }
+
+        // MARK: LOCK END
     }
 }
--- Tests/SwiftThrottleTests/SwiftThrottleTests.swift
+++ Tests/SwiftThrottleTests/SwiftThrottleTests.swift
@@ -2,7 +2,7 @@
 import XCTest
 
 /// Indicates how long the test will run
-let multithreadThreshold = 30
+let multithreadThreshold = 15
 
 final class SwiftThrottleTests: XCTestCase {
     /// Test the throttle
@@ -13,7 +13,7 @@ final class SwiftThrottleTests: XCTestCase {
 
         let sem = DispatchSemaphore(value: 0)
         let totalTestTime = Double(multithreadThreshold)
-        let emitter: Double = 0.1
+        let emitter: Double = 0.5
 
         // controller
         DispatchQueue.global(qos: .background).async {
@@ -26,7 +26,7 @@ final class SwiftThrottleTests: XCTestCase {
             print("Terminating throttle test...")
             testShouldTerminate = true
             print("Waiting for ending expectation assertions to arrive...")
-            sleep(5)
+            sleep(3)
             sem.signal()
         }
 
@@ -34,28 +34,34 @@ final class SwiftThrottleTests: XCTestCase {
         let results_lock = NSLock()
         DispatchQueue.global(qos: .background).async {
             let cpuCount = ProcessInfo().processorCount
-            XCTAssert(cpuCount > 0,
-                      "this test must be performed on a machine that has at least 1 cpu core")
+            XCTAssert(cpuCount > 0, "this test must be performed on a machine that has at least 1 cpu core")
 
             var dispatch = 0
             while dispatch < cpuCount {
                 // worker
-
-                let name = "ThrottleTest.\(dispatch)"
+                let currentDispatchIndex = dispatch + 1
+                let name = "ThrottleTest.\(currentDispatchIndex)"
                 let queue = DispatchQueue(label: name, attributes: .concurrent) // just in case
-                let throttle = Throttle(minimumDelay: emitter, queue: DispatchQueue(label: name + ".worker"))
+                let throttle = Throttle(minimumDelay: Double.random(in: 0 ... 0.1), queue: DispatchQueue(label: name + ".worker"))
                 print("Throttle test is starting test thread \(name)")
+                throttle.updateMinimumDelay(interval: emitter)
+                throttle.throttle(job: nil)
                 queue.async {
                     var hit = 0
                     while !testShouldTerminate {
                         throttle.throttle { hit += 1 }
+                        // with this sleep follow we get:
+                        // Throttle executed for [220, 220, 222, 216, 219, 221, 220, 221] times
+                        usleep(useconds_t.random(in: 0 ... 1000))
                     }
                     var endingExpectation = ""
                     let expectValue = "ending.call.successed"
-                    throttle.throttle { endingExpectation = expectValue }
-                    usleep(UInt32(emitter * 1_000_000) * 2)
-                    XCTAssert(endingExpectation == expectValue,
-                              "Throttle failed to program last execution")
+                    throttle.throttle {
+                        print("Throttle on thread \(currentDispatchIndex) setting endingExpectation...")
+                        endingExpectation = expectValue
+                    }
+                    sleep(1)
+                    XCTAssert(endingExpectation == expectValue, "Throttle failed to program last execution on thread: \(currentDispatchIndex)")
                     results_lock.lock()
                     results.append(hit)
                     results_lock.unlock()
@@ -74,25 +80,21 @@ final class SwiftThrottleTests: XCTestCase {
 
         /*
 
-         ---
+         ** ABOUT GCD **
 
+         if we dont sleep there we get:
+         -> usleep(useconds_t.random(in: 0 ... 1000))
+
+         ---
          Throttle thread safe test completed
          Throttle executed for [303, 303, 303, 0, 0, 0, 0, 303] times
          Throttle test completed
-         Test Case '-[SwiftThrottleTests.SwiftThrottleTests test]' passed (31.281 seconds).
-         Test Suite 'SwiftThrottleTests' passed at 2021-07-04 17:52:11.745.
-              Executed 1 test, with 0 failures (0 unexpected) in 31.281 (31.281) seconds
-         Test Suite 'SwiftThrottleTests.xctest' passed at 2021-07-04 17:52:11.747.
-              Executed 1 test, with 0 failures (0 unexpected) in 31.281 (31.283) seconds
-         Test Suite 'Selected tests' passed at 2021-07-04 17:52:11.748.
-              Executed 1 test, with 0 failures (0 unexpected) in 31.281 (31.284) seconds
-
          ---
 
          -> Throttle executed for [303, 303, 303, 0, 0, 0, 0, 303] times
 
          this shows that you will still need to handle GCD concurrency in your app in a right way
-         this is not a bug
+         this is not a bug and do not use my throttle in a loop without giving up cpu time
 
          */
     }
